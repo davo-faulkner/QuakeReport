ef28fadc94050d21370e47b82a3f721c78cfaee2
Davo Faulkner <d@davo.co>
2017-03-07 17:05:08 -0600

    Add global url variable to EarthquakeLoader (no compile errors)

--- app/src/main/java/EarthquakeLoader.java
+++ app/src/main/java/EarthquakeLoader.java
@@ -2,18 +2,21 @@ import android.content.AsyncTaskLoader;
 import android.content.Context;
 
 import com.example.android.quakereport.Earthquake;
+import com.example.android.quakereport.QueryUtils;
 
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by Davo on 3/7/2017.
  */
 
-public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>>{
+public class EarthquakeLoader extends AsyncTaskLoader<ArrayList<Earthquake>> {
+    private static String USGS_REQUEST_URL = null;
 
     public EarthquakeLoader(Context context, String url) {
         super(context);
         // TODO: Finish implementing this constructor
+        USGS_REQUEST_URL = url;
     }
 
     @Override
@@ -22,7 +25,9 @@ public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>>{
     }
 
     @Override
-    public List<Earthquake> loadInBackground() {
+    public ArrayList<Earthquake> loadInBackground() {
         // TODO: Implement this method
+        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes(USGS_REQUEST_URL);
+        return earthquakes;
     }
 }
